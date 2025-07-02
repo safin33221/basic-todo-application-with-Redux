f1a761f318ebb929038ad66401349919740dcf3d
safin33221 <safin33221@gmail.com>
2025-07-02 15:20:30 +0600

    Update Task and Deleted task functionality completed

--- src/Pages/Task.tsx
+++ src/Pages/Task.tsx
@@ -1,6 +1,9 @@
-import {  AddTaskModal } from "@/components/ui/module/AddTask";
-import { selectTasks } from "@/redux/features/task/taskSlice";
-import { useAppSelector } from "@/redux/hooks";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { AddTaskModal } from "@/components/ui/module/AddTask";
+import { deleteTask, selectTasks, toggleCompleteState } from "@/redux/features/task/taskSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { Trash2 } from "lucide-react";
 
 const Task = () => {
     const tasks = useAppSelector(selectTasks);
@@ -11,7 +14,7 @@ const Task = () => {
         MEDIUM: 'bg-yellow-500',
         LOW: 'bg-green-500',
     };
-
+    const dispatch = useAppDispatch()
     return (
         <div className="space-y-4">
 
@@ -21,14 +24,26 @@ const Task = () => {
                     key={task.id}
                     className="p-4 rounded-2xl shadow-lg dark:bg-gray-900 text-white w-full max-w-full mx-auto"
                 >
-                    <div className="flex items-start gap-4">
-                        <span
-                            className={` p-1 text-sm rounded-full ${priorityColor[task.priority] || 'bg-gray-500'
-                                }`}
-                        >
+                    <div className="flex justify-between gap-4">
+                        <div className=" flex items-start">
 
-                        </span>
-                        <h2 className="text-xl font-semibold">{task?.title}</h2>
+                            <span
+                                className={` p-1 text-sm rounded-full ${priorityColor[task.priority] || 'bg-gray-500'
+                                    }`}
+                            >
+
+                            </span>
+                            <h2 className="text-xl font-semibold">{task?.title}</h2>
+                        </div>
+
+                        <div>
+                            <Checkbox
+                            checked={task.isCompleted} 
+                            onClick={() => dispatch(toggleCompleteState(task.id))} />
+                            <Button className="mx-2" onClick={() => dispatch(deleteTask(task.id))}>
+                                <Trash2 />
+                            </Button>
+                        </div>
 
                     </div>
                     <p className="mt-2 text-sm text-gray-300">{task.description}</p>
--- src/redux/features/task/taskSlice.ts
+++ src/redux/features/task/taskSlice.ts
@@ -1,31 +1,45 @@
 import type { RootState } from "@/redux/store";
 import type { ITask } from "@/types";
-import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
-import { v4 as uuidv4 } from 'uuid';
+import { createSlice, nanoid, type PayloadAction } from "@reduxjs/toolkit";
+
 interface InitialState {
     tasks: ITask[],
     filter: "ALL" | "HIGH" | "MEDIUM" | "LOW"
 }
 const initialState: InitialState = {
-    tasks: [],
+    tasks: [
+        {
+            "id": "hy_979YXuqtlHqImvCmPK",
+            "isCompleted": false,
+            "title": "Optio ea dolore ali",
+            "description": "Qui in blanditiis po",
+            "priority": "LOW",
+            "dueDate": "2025-07-17T18:00:00.000Z",
+        }
+    ],
     filter: "ALL"
 }
 
 
 
 type DraftTask = Pick<ITask, "title" | "description" | "dueDate" | "priority">
+
+const createTask = (taskData: DraftTask): ITask => {
+    return { id: nanoid(), isCompleted: false, ...taskData }
+}
 const taskSlice = createSlice({
     name: "tasks",
     initialState,
     reducers: {
         addTask: (state, action: PayloadAction<ITask>) => {
-            const id = uuidv4()
-            const task = {
-                ...action.payload,
-                id,
-                isCompleted: false
-            }
+            const task = createTask(action.payload)
             state.tasks.push(task)
+        },
+        toggleCompleteState: (state, action: PayloadAction<string>) => {
+            state.tasks.forEach((task) => task.id === action.payload ? task.isCompleted = !task.isCompleted : task)
+        },
+        deleteTask: (state, action: PayloadAction<string>) => {
+            state.tasks = state.tasks.filter(task => task.id !== action.payload)
         }
     }
 })
@@ -38,7 +52,7 @@ export const selectFilter = (state: RootState) => {
     return state.todos.tasks
 }
 
-export const { addTask } = taskSlice.actions
+export const { addTask, toggleCompleteState,deleteTask } = taskSlice.actions
 
 export default taskSlice.reducer
 
